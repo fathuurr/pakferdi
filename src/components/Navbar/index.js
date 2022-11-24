19b8186516b1761173bcd8ccdb4cae2173fd6a3c
Fathurrahman <41844599+fathuurr@users.noreply.github.com>
2022-11-24 17:04:19 +0700

    initial commit

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function Navbar() {
+  return (
+    <h1 className="text-center text-4xl text-green-400">
+      Ini Navbar baco lu kontol semua
+    </h1>
+  );
+}
+
+export default Navbar;
