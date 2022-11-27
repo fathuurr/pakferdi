9868e86a3460e407eb6846cd106d82c5843ba59e
gavansatriahastoro <gvnjr11@gmail.com>
2022-11-27 14:35:38 +0700

    enhance: add pages with functional components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import LandingPage from './pages/LandingPage';
 import LogIn from './pages/LogIn';
 
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Router>
         <Routes>
           <Route exact path="/" element={<LandingPage />} />
+          <Route exace path="/home" element={<Home />} />
           <Route exact path="/sign-in" element={<LogIn />} />
         </Routes>
       </Router>
