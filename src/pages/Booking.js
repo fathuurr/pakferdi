b01caf3e251edcddef5f7572b90618096be44e9b
Fathurrahman <41844599+fathuurr@users.noreply.github.com>
2022-12-12 00:54:54 +0700

    add loading to all page

--- src/pages/Booking.js
+++ src/pages/Booking.js
@@ -1,29 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
+import ReactLoading from "react-loading";
 
-import Navbar from '../components/Navbar';
-import Footer from '../parts/Footer';
-import { Link } from 'react-router-dom';
+import Navbar from "../components/Navbar";
+import Footer from "../parts/Footer";
+import { Link } from "react-router-dom";
 
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 
-import { ReactComponent as Male } from '../assets/male.svg';
-import { ReactComponent as Female } from '../assets/female.svg';
-import { ReactComponent as IconBack } from '../assets/icon-back.svg';
+import { ReactComponent as Male } from "../assets/male.svg";
+import { ReactComponent as Female } from "../assets/female.svg";
+import { ReactComponent as IconBack } from "../assets/icon-back.svg";
 
-import Select from 'react-select';
+import Select from "react-select";
 const selectService = [
-  { value: 'Pijat Aroma Terapi', label: 'Pijat Aroma Terapi' },
-  { value: 'Pijat Batu Panas', label: 'Pijat Batu Panas' },
-  { value: 'Pijat Thai', label: 'Pijat Thai' },
-  { value: 'Pijat Refleksi', label: 'Pijat Refleksi' },
-  { value: 'Pijat Olahraga', label: 'Pijat Olahraga' },
-  { value: 'Pijat Lulur', label: 'Pijat Lulur' },
+  { value: "Pijat Aroma Terapi", label: "Pijat Aroma Terapi" },
+  { value: "Pijat Batu Panas", label: "Pijat Batu Panas" },
+  { value: "Pijat Thai", label: "Pijat Thai" },
+  { value: "Pijat Refleksi", label: "Pijat Refleksi" },
+  { value: "Pijat Olahraga", label: "Pijat Olahraga" },
+  { value: "Pijat Lulur", label: "Pijat Lulur" },
 ];
 
 const Booking = () => {
   const [date, setDate] = useState(new Date());
+  const [loading, setLoading] = useState(false);
+
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyDw6Rj1YiUjCpmgZvCt9qrJladyMcUdN5g",
+    libraries: ["geometry", "drawing"],
+  });
 
   const center = {
     lat: -6.2269976,
@@ -31,115 +39,135 @@ const Booking = () => {
   };
 
   const containerStyle = {
-    width: '90%',
-    height: '300px',
-    position: 'relative',
-    left: '50%',
-    transform: 'translateX(-50%)',
-    borderRadius: '20px',
+    width: "75%",
+    height: "300px",
+    position: "relative",
+    left: "50%",
+    transform: "translateX(-50%)",
+    borderRadius: "20px",
   };
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
 
   return (
     <>
       <Navbar />
 
-      <div class="m-booking">
-        <Link to="/" class="iconBack">
-          <IconBack />
-        </Link>
+      {loading ? (
+        <ReactLoading
+          className="absolute top-1/2 left-1/2"
+          type={"bubbles"}
+          color={"blue"}
+          height={64}
+          width={64}
+        />
+      ) : (
+        <>
+          <div class="m-booking">
+            <Link to="/" class="iconBack">
+              <IconBack />
+            </Link>
 
-        <h1> Pilih Jenis Pijat </h1>
-        <div class="select">
-          <Select
-            styles={{
-              control: (baseStyles, state) => ({
-                ...baseStyles,
-                borderColor: 'white',
-              }),
-            }}
-            placeholder="Pilih Jenis Pijat"
-            options={selectService}
-          />
-        </div>
+            <h1> Pilih Jenis Pijat </h1>
+            <div class="select">
+              <Select
+                styles={{
+                  control: (baseStyles, state) => ({
+                    ...baseStyles,
+                    borderColor: "white",
+                  }),
+                }}
+                placeholder="Pilih Jenis Pijat"
+                options={selectService}
+              />
+            </div>
 
-        <h1> Pilih Gender Anda </h1>
-        <div class="pilihService">
-          <div class="service">
-            <Male />
-            <p>Pria</p>
+            <h1> Pilih Gender Anda </h1>
+            <div class="pilihService">
+              <div class="service">
+                <Male />
+                <p>Pria</p>
+              </div>
+              <div class="service">
+                <Female />
+                <p>Perempuan</p>
+              </div>
+            </div>
+
+            <h1> Pilih Durasi </h1>
+            <div class="pilihService">
+              <div class="service">
+                <p>
+                  30 Menit - <i>Rp. 60.000</i>{" "}
+                </p>
+              </div>
+              <div class="service">
+                <p>
+                  60 Menit - <i>Rp. 120.000</i>{" "}
+                </p>
+              </div>
+              <div class="service">
+                <p>
+                  90 Menit - <i>Rp. 180.000</i>{" "}
+                </p>
+              </div>
+              <div class="service">
+                <p>
+                  120 Menit - <i>Rp. 240.000</i>{" "}
+                </p>
+              </div>
+            </div>
+
+            <h1> Pilih Gender Terapis </h1>
+            <div class="pilihService">
+              <div class="service">
+                <Male />
+                <p>Pria</p>
+              </div>
+              <div class="service">
+                <Female />
+                <p>Perempuan</p>
+              </div>
+            </div>
+
+            <h1> Pilih Waktu </h1>
+            <div className="calendar-container">
+              <Calendar onChange={setDate} value={date} />
+            </div>
+
+            <h1> Pilih Lokasi </h1>
+
+            {isLoaded && (
+              <GoogleMap
+                mapContainerStyle={containerStyle}
+                center={center}
+                zoom={12}
+              />
+            )}
+
+            <h2> Alamat Lengkap :</h2>
+            <div class="alamatLengkap">
+              <input placeholder="Provinsi, Kota, Kecamatan, Kode Pos"></input>
+              <input placeholder="Nama Jalan, Gedung, No. Rumah"></input>
+              <input placeholder="Detail Lainnya (Cth: Blok/Unit No., Patokan)"></input>
+            </div>
+
+            <Link
+              className="flex justify-center mx-auto mt-10 w-full bg-[#466FC1] text-white rounded-lg py-4 max-sm:w-1/2 text-sm"
+              to={"/payment"}
+            >
+              Selanjutnya
+            </Link>
           </div>
-          <div class="service">
-            <Female />
-            <p>Perempuan</p>
-          </div>
-        </div>
 
-        <h1> Pilih Durasi </h1>
-        <div class="pilihService">
-          <div class="service">
-            <p>
-              30 Menit - <i>Rp. 60.000</i>{' '}
-            </p>
-          </div>
-          <div class="service">
-            <p>
-              60 Menit - <i>Rp. 120.000</i>{' '}
-            </p>
-          </div>
-          <div class="service">
-            <p>
-              90 Menit - <i>Rp. 180.000</i>{' '}
-            </p>
-          </div>
-          <div class="service">
-            <p>
-              120 Menit - <i>Rp. 240.000</i>{' '}
-            </p>
-          </div>
-        </div>
-
-        <h1> Pilih Gender Terapis </h1>
-        <div class="pilihService">
-          <div class="service">
-            <Male />
-            <p>Pria</p>
-          </div>
-          <div class="service">
-            <Female />
-            <p>Perempuan</p>
-          </div>
-        </div>
-
-        <h1> Pilih Waktu </h1>
-        <div className="calendar-container">
-          <Calendar onChange={setDate} value={date} />
-        </div>
-
-        <h1> Pilih Lokasi </h1>
-        <LoadScript
-          googleMapsApiKey={'AIzaSyDw6Rj1YiUjCpmgZvCt9qrJladyMcUdN5g'}>
-          <GoogleMap
-            mapContainerStyle={containerStyle}
-            center={center}
-            zoom={12}
-          />
-        </LoadScript>
-
-        <h2> Alamat Lengkap :</h2>
-        <div class="alamatLengkap">
-          <input placeholder="Provinsi, Kota, Kecamatan, Kode Pos"></input>
-          <input placeholder="Nama Jalan, Gedung, No. Rumah"></input>
-          <input placeholder="Detail Lainnya (Cth: Blok/Unit No., Patokan)"></input>
-        </div>
-
-        <Link
-          className="flex justify-center mx-auto mt-10 w-full bg-[#466FC1] text-white rounded-lg py-4 max-sm:w-1/2 text-sm"
-          to={'/payment'}>
-          Selanjutnya
-        </Link>
-      </div>
-
-      <Footer />
+          <Footer />
+        </>
+      )}
     </>
   );
 };
--- src/pages/DetailBooking.js
+++ src/pages/DetailBooking.js
@@ -1,66 +1,88 @@
-import Navbar from '../components/Navbar';
-import Footer from '../parts/Footer';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import ReactLoading from "react-loading";
+import { Link } from "react-router-dom";
+
+import Navbar from "../components/Navbar";
+import Footer from "../parts/Footer";
 
 const DetailBooking = () => {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
   return (
     <>
       <Navbar />
+      {loading ? (
+        <ReactLoading
+          className="absolute top-1/2 left-1/2"
+          type={"bubbles"}
+          color={"blue"}
+          height={64}
+          width={64}
+        />
+      ) : (
+        <>
+          <div class="m-booking">
+            <h1> Detail Booking </h1>
+            <div class="divpay">
+              <img class="checkpayment" src="check.svg" alt="" />
 
-      <div class="m-booking">
-        <h1> Detail Booking </h1>
-        <div class="divpay">
-          <img class="checkpayment" src="check.svg" alt="" />
+              <table class="tableConfirm">
+                <tr>
+                  <td>Nama &nbsp;</td>
+                  <td> : Jainudin</td>
+                </tr>
+                <tr>
+                  <td>Jenis Layanan Pijat &nbsp;</td>
+                  <td> : Pijat Aromaterapi</td>
+                </tr>
+                <tr>
+                  <td>Durasi &nbsp;</td>
+                  <td> : 60 Menit</td>
+                </tr>
+                <tr>
+                  <td>Gender Terapis &nbsp;</td>
+                  <td> : Pria</td>
+                </tr>
+                <tr>
+                  <td>Waktu &nbsp;</td>
+                  <td> : 12:00 pm</td>
+                </tr>
+                <tr>
+                  <td>Lokasi &nbsp;</td>
+                  <td> : Jakarta</td>
+                </tr>
+                <tr>
+                  <td>Status Pembayaran &nbsp;</td>
+                  <td>
+                    :
+                    <label>
+                      <b>
+                        <u> Berhasil </u>
+                      </b>
+                    </label>
+                  </td>
+                </tr>
+              </table>
+            </div>
 
-          <table class="tableConfirm">
-            <tr>
-              <td>Nama &nbsp;</td>
-              <td> : Jainudin</td>
-            </tr>
-            <tr>
-              <td>Jenis Layanan Pijat &nbsp;</td>
-              <td> : Pijat Aromaterapi</td>
-            </tr>
-            <tr>
-              <td>Durasi &nbsp;</td>
-              <td> : 60 Menit</td>
-            </tr>
-            <tr>
-              <td>Gender Terapis &nbsp;</td>
-              <td> : Pria</td>
-            </tr>
-            <tr>
-              <td>Waktu &nbsp;</td>
-              <td> : 12:00 pm</td>
-            </tr>
-            <tr>
-              <td>Lokasi &nbsp;</td>
-              <td> : Jakarta</td>
-            </tr>
-            <tr>
-              <td>Status Pembayaran &nbsp;</td>
-              <td>
-                {' '}
-                :{' '}
-                <label>
-                  {' '}
-                  <b>
-                    <u> Berhasil </u>
-                  </b>{' '}
-                </label>{' '}
-              </td>
-            </tr>
-          </table>
-        </div>
+            <Link
+              className="flex justify-center mx-auto mt-10 w-full bg-[#466FC1] text-white rounded-lg py-4 max-sm:w-1/2 text-sm"
+              to={"/"}
+            >
+              Kembali ke Beranda
+            </Link>
+          </div>
 
-        <Link
-          className="flex justify-center mx-auto mt-10 w-full bg-[#466FC1] text-white rounded-lg py-4 max-sm:w-1/2 text-sm"
-          to={'/'}>
-          Kembali ke Beranda
-        </Link>
-      </div>
-
-      <Footer />
+          <Footer />
+        </>
+      )}
     </>
   );
 };
