9e39636bd8dfb8b71eeeb0b27c2cbcc4749a08e4
Fathurrahman <41844599+fathuurr@users.noreply.github.com>
2022-12-11 23:18:46 +0700

    loading

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,20 +1,42 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
-import Hero from '../parts/Hero';
-import Services from '../parts/Services';
-import Review from '../parts/Review';
-import About from '../parts/About';
-import Footer from '../parts/Footer';
+import React, { useState, useEffect } from "react";
+import ReactLoading from "react-loading";
+
+import Navbar from "../components/Navbar";
+import Hero from "../parts/Hero";
+import Services from "../parts/Services";
+import Review from "../parts/Review";
+import About from "../parts/About";
+import Footer from "../parts/Footer";
 
 function LandingPage() {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
   return (
     <>
-      <Navbar />
-      <Hero />
-      <Services />
-      <Review />
-      <About />
-      <Footer />
+      {loading ? (
+        <ReactLoading
+          className="absolute top-1/2 left-1/2"
+          type={"bubbles"}
+          color={"blue"}
+          height={64}
+          width={64}
+        />
+      ) : (
+        <>
+          <Navbar />
+          <Hero />
+          <Services />
+          <Review />
+          <About />
+          <Footer />
+        </>
+      )}
     </>
   );
 }
