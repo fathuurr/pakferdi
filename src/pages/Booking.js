d70e78c7af488321411070e0957b2f18e21b87e3
Fathurrahman <41844599+fathuurr@users.noreply.github.com>
2022-12-18 21:07:17 +0700

    fikri : fix button harga

--- src/pages/Booking.js
+++ src/pages/Booking.js
@@ -14,6 +14,7 @@ import { ReactComponent as Female } from '../assets/female.svg';
 import { ReactComponent as IconBack } from '../assets/icon-back.svg';
 
 import Select from 'react-select';
+
 const selectService = [
   { value: 'Pijat Aroma Terapi', label: 'Pijat Aroma Terapi' },
   { value: 'Pijat Batu Panas', label: 'Pijat Batu Panas' },
@@ -68,12 +69,12 @@ const Booking = () => {
         />
       ) : (
         <>
-          <div className="container mx-auto px-8 mt-10">
+          <div className="m-booking">
             <Link to="/" className="iconBack">
               <IconBack />
             </Link>
 
-            <h1 className="mt-10"> Pilih Jenis Pijat </h1>
+            <h1> Pilih Jenis Pijat </h1>
             <div className="select">
               <Select
                 styles={{
@@ -87,55 +88,87 @@ const Booking = () => {
               />
             </div>
 
-            <h1 className="mt-10"> Pilih Gender Anda </h1>
+            <h1> Pilih Gender Anda </h1>
             <div className="pilihService mt-10">
-              <div className="service">
+              <input type="radio" name="groupA" id="T35" />
+              <label
+                class="ButtonBook"
+                for="T35"
+                onClick="() => UpdateTheChart(11)">
                 <Male />
                 <p>Pria</p>
-              </div>
-              <div className="service">
+              </label>
+              <input type="radio" name="groupA" id="T36"></input>
+              <label
+                class="ButtonBook"
+                for="T36"
+                onClick="() => UpdateTheChart(12)">
                 <Female />
                 <p>Perempuan</p>
-              </div>
+              </label>
             </div>
 
-            <h1 className="mt-10"> Pilih Durasi </h1>
+            <h1> Pilih Durasi </h1>
             <div className="pilihService mt-10">
-              <div className="service">
+              <input type="radio" name="groupB" id="T41" />
+              <label
+                class="ButtonBook"
+                for="T41"
+                onClick="() => UpdateTheChart(21)">
                 <p>
-                  40 Menit - <i>Rp. 130.000</i>
+                  30 Menit - <i>Rp. 60.000</i>{' '}
                 </p>
-              </div>
-              <div className="service">
+              </label>
+              <input type="radio" name="groupB" id="T42" />
+              <label
+                class="ButtonBook"
+                for="T42"
+                onClick="() => UpdateTheChart(22)">
                 <p>
-                  60 Menit - <i>Rp. 160.000</i>
+                  60 Menit - <i>Rp. 120.000</i>{' '}
                 </p>
-              </div>
-              <div className="service">
+              </label>
+              <input type="radio" name="groupB" id="T43" />
+              <label
+                class="ButtonBook"
+                for="T43"
+                onClick="() => UpdateTheChart(23)">
                 <p>
-                  90 Menit - <i>Rp. 200.000</i>
+                  90 Menit - <i>Rp. 180.000</i>{' '}
                 </p>
-              </div>
-              <div className="service">
+              </label>
+              <input type="radio" name="groupB" id="T44" />
+              <label
+                class="ButtonBook"
+                for="T44"
+                onClick="() => UpdateTheChart(24)">
                 <p>
-                  120 Menit - <i>Rp. 300.000</i>
+                  120 Menit - <i>Rp. 240.000</i>{' '}
                 </p>
-              </div>
+              </label>
             </div>
 
-            <h1 className="mt-10"> Pilih Gender Terapis </h1>
+            <h1> Pilih Gender Terapis </h1>
             <div className="pilihService mt-10">
-              <div className="service">
+              <input type="radio" name="groupC" id="T51" />
+              <label
+                class="ButtonBook"
+                for="T51"
+                onClick="() => UpdateTheChart(31)">
                 <Male />
                 <p>Pria</p>
-              </div>
-              <div className="service">
+              </label>
+              <input type="radio" name="groupC" id="T52" />
+              <label
+                class="ButtonBook"
+                for="T52"
+                onClick="() => UpdateTheChart(32)">
                 <Female />
                 <p>Perempuan</p>
-              </div>
+              </label>
             </div>
 
-            <h1 className="mt-10"> Pilih Tanggal & Waktu </h1>
+            <h1> Pilih Tanggal & Waktu </h1>
             <div className="calendar-container mt-10">
               <Calendar onChange={setDate} value={date} />
               <input
@@ -156,7 +189,7 @@ const Booking = () => {
               />
             )}
 
-            <h2 className="mt-10"> Alamat Lengkap :</h2>
+            <h2> Alamat Lengkap :</h2>
             <div className="alamatLengkap mt-10">
               <input placeholder="Provinsi, Kota, Kecamatan, Kode Pos"></input>
               <input placeholder="Nama Jalan, Gedung, No. Rumah"></input>
